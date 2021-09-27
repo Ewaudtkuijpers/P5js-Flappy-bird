2ab29584d1357b018ab633dc9e06d2cae94bc109
Ewoud Kuijpers <74767424+Ewaudtkuijpers@users.noreply.github.com>
2021-09-27 11:19:41 +0000

    pipes onderkant

--- script.js
+++ script.js
@@ -1,8 +1,37 @@
+var x,y
+
+class Pipe{
+  constructor(x,y,w,h,vx){
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+    this.vx = vx;
+  }
+
+  show () {
+    rect(this.x,this.y,this.w,this.h);
+    this.x = this.x - 2;
+  }
+}
+
+var pipes = [];
+
 function setup() {
-  createCanvas(500,500);
-  background(0,225,250);
+  createCanvas(500,300);  
 }
 
 function draw() {
-  ellipse(200,100,40,40);
+  background(0,225,250);
+
+  if(frameCount % 100 == 0){
+    console.log("nieuwe pipe!");
+    console.log(pipes.length);
+    
+    pipe = new Pipe(500,250,30,50,0);
+    pipes.push(pipe);
+  }
+
+  pipes.forEach(p=> p.show());
 }
+
